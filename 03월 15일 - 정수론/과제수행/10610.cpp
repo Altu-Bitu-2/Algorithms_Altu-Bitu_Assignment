fa728ebea9cbb963abc3caba93931ffabd69da91
Cse pebb <cse_pebb@naver.com>
2022-03-21 12:43:53 +0900

    [정수론] 3월 21일 -ing

--- 03월 15일 - 정수론/과제수행/10610.cpp
+++ 03월 15일 - 정수론/과제수행/10610.cpp
@@ -1,22 +1,15 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//내림차순 cmp 함수 
-bool cmp(string s1, string s2){
-	return s1 > s2;	//string 사전 역순 
-}
-
 //30의 배수인 경우 가장 큰 30의 배수를 찾아서 return하는 함수 
-int findGreat30(string n,vector<int> &v) { //각 자릿수에 접근해야 하므로 n을 인자로 받기 
-	int great_num; 
-	vector<int> temp;
-	
-	//맨 마지막 숫자는 제외하고 내림차순 정렬 
-	//맨 끝자리는 무조건 0 
-
+int findGreat30(vector<int> &v) { 
+	//내림차순으로 정렬하면 최댓값 찾을 수 있음. 맨 뒤는 항상 0
+	int great_num = sort(v.begin(), v.end(), greater<>()) //!컴파일 에러 
 	return great_num; 
+	//return -2; //디버깅용
 }
 
 //30의 배수인지 확인해주는 함수 
@@ -28,7 +21,7 @@ int is30(string n, vector<int> &v){
 		sum += v[i]; //각 자릿수의 합
 		if (sum % 3 != 0) return -1; //합이 3의 배수가 아닐 경우
 		//2. 각 자릿수의 숫자들 중 적어도 1개가 0이면(10의 배수 조건) 30의 배수 존재 
-		if (v[i]==0) return findGreat30(n,v); //만들 수 있는 가장 큰 30의 배수 return 
+		if (v[i]==0) return findGreat30(v); //만들 수 있는 가장 큰 30의 배수 return 
 	}
 	return -1; //0이 하나도 없으면 반복문 빠져나와서 return -1
 }
@@ -41,10 +34,8 @@ int main(){
 	for (int i=0;i<n.length();i++){
 		int temp = n[i] - '0'; //각 자릿수로 나누고 정수형으로 형 변환 
 		v.push_back(temp); 	// v에 각자리수 저장
-		
 	} 
 	
 	//출력 
-	cout << is30(n,v); 
-	 
+	cout << is30(v); 
 } 	       
