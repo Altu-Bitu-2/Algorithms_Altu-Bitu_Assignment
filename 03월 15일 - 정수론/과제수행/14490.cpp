ccb8918c49317cea085d2088edf9a652c0218b4b
Cse pebb <cse_pebb@naver.com>
2022-03-22 21:25:20 +0900

    [정수론] 3월 22일
    Update 14490.cpp

--- 03월 15일 - 정수론/과제수행/14490.cpp
+++ 03월 15일 - 정수론/과제수행/14490.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 const int LENGTH = 18; //:를 뺀 a와 b의 총 길이. a, b 각각 9자리가 최대이므로 최대 18자리 
+int int_a = 0, int_b = 0; // 정수형으로 변환한 두 수 a, b 
 
 //GCD 재귀함수 : 최대공약수를 return 
 int gcdRecursion(int a, int b) {
@@ -17,25 +18,18 @@ int divideAndStoi(string& only_num, int only_num_length, int& a_length, int& b_l
 	//a, b 나누기 
 	for (int i = 0; i < a_length; i++) { //a만 저장 
 		string_a[i] = only_num[i];
-		cout << string_a[i]; //!a를 하나하나 출력해보자
 	}
-	cout << "\n";
+	int temp = 0; //string_b의 현재 인덱스를 저장할 변수 
 	for (int i = a_length; i < only_num_length; i++) { //b만 저장 
-		int temp = 0; //string_b의 현재 인덱스를 저장할 변수 
 		string_b[temp] = only_num[i];
-		cout << string_b[temp]; //!b를 하나하나 출력해보자 
 		temp++;	//string_b의 인덱스 +1 업데이트 
 	}
-	cout << "\n";
 
 	//string_a, string_b를 각각 정수로 형 변환 해주기 
-	cout << "b의 길이는 " << b_length << "\n"; //!string에서는 분명 b의 길이가 정상출력됨 
-	int a = stoi(string_a);
-	cout << "stoi한  a는 " << a << "\n"; //!a는 stoi가 정상적으로 됨 
-	int b = stoi(string_b);
-	cout << "stoi한 b는 " << b; //!하지만 b가 이상하게 나옴.. 
+	int_a = stoi(string_a);
+	int_b = stoi(string_b);
 
-	return gcdRecursion(a, b); //!a, b의 최대공약수를 return해준다 
+	return gcdRecursion(int_a, int_b); //!a, b의 최대공약수를 return해준다 
 }
 
 int main() {
@@ -52,11 +46,6 @@ int main() {
 		if (s[i] != ':') { // :가 아니라 숫자가 들어오면  
 			only_num[index++] = s[i]; //숫자 하나 넣어준 후 index +1 업데이트 
 			cnt++; //a의 길이를 세주기 위해 cnt +1 업데이트. 
-/*
-			only_num[index] = s[i];
-			cout << only_num[index];
-			index++;
-			cnt++;  */ // 디버깅용 
 		}
 		else { //a의 길이를 세다가 :를 만났을 경우가 최종 a의 길이 
 			a_length = cnt;
@@ -68,8 +57,11 @@ int main() {
 	//실제 a와 b의 길이의 합으로 업데이트 해주기
 	int only_num_length = a_length + b_length;
 
+	//ㅁ, b의 최대공약수 구하기 
 	int gcd = divideAndStoi(only_num, only_num_length, a_length, b_length);
-	//cout << a << ':' << b;
+
+	// a, b를 최대공약수르 나눠서 출력 
+	cout << int_a / gcd << ':' << int_b / gcd;
 
 	return 0;
 }
