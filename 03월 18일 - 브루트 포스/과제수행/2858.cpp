d95a40af01816f7bc00259dcfbbec6360fdd9435
Cse pebb <cse_pebb@naver.com>
2022-03-24 23:59:18 +0900

    [브루트 포스] 3월 24일 -ing
    2858.cpp

---
+++ 03월 18일 - 브루트 포스/과제수행/2858.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 총 타일의 개수(R+B)의 약수 쌍 구하기
+int findDivisor(int r, int b){
+	int sum = r+b;
+	
+	// 약수 쌍 구하기  
+	for (int i=3; i<=sum/2; i++) { // 타일의 행이 3보다 작으면 B가 존재할 수 X. 따라서 i=3부터, 중복을 피하기 위해 sum/2 까지 
+		
+	}
+} 
+ 
+int main() {
+	// 입력
+	int r,b; 
+	cin >> r >> b;
+	
+	int divisor = findDivisor(r,b);
+	
+	return 0;
+} 
