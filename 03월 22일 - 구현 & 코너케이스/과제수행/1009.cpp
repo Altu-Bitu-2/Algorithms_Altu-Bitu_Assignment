cf278123f7cd082bf3510e6c64a0df25266a4e3c
CSE-pebble <CSE.pebble@gmail.com>
2022-03-29 01:35:28 +0900

    [구현 & 코너케이스] 3월 29일 -ing
    Update 1009.cpp

--- 03월 22일 - 구현 & 코너케이스/과제수행/1009.cpp
+++ 03월 22일 - 구현 & 코너케이스/과제수행/1009.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 // 마지막 데이터가 처리되는 컴퓨터의 번호를 찾아주는 함수
 int findComNum(int a, int b, int cnt) {
-	if (a == 10) // a가 10일 때는
+	if (a % 10 == 0) // 일의 자리가 0일 때는
 		return 10; // 항상 10번째
 	if (cnt == 1) // cnt가 1일 때는 b번이 아무리 커도 모든 일의 자리가 a의 일의 자리와 같음. 즉 마지막 데이터의 일의 자리수는 항상 a % 10.
-		return a % 10 % 10; 
+		return a % 10 % 10;
 	int a_pow = b % cnt; // a를 b번 곱했을 때(즉 마지막 데이터)의 일의 자리수 구하기
 	return (int)pow(a, a_pow) % 10;
 }
@@ -18,7 +18,7 @@ int findIterA(int a) {
 	int temp = a % 10; // a 자체의 일의 자리 
 	int cnt = 1; // 반복횟수 count
 	while (true) {
-		temp *= a; // a 곱해주기
+		temp *= (a % 10); // a의 일의 자리 곱해주기
 		temp %= 10; // 곱한 수의 일의 자리 구하기
 		if (temp == a % 10) return cnt; // a 자체의 일의 자리로 돌아왔다면 반복횟수 return 
 		cnt++; // 반복횟수 업데이트 
