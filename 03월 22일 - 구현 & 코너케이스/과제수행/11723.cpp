d5dfdfd3d1c6820282d5eb76834a0c589ccb0b46
CSE-pebble <CSE.pebble@gmail.com>
2022-03-29 01:34:22 +0900

    [구현 & 코너케이스] 3월 28일 -ing
    11723.cpp

--- 03월 22일 - 구현 & 코너케이스/과제수행/11723.cpp
+++ 03월 22일 - 구현 & 코너케이스/과제수행/11723.cpp
@@ -1,74 +1,104 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> s;
 
+bool check(int x) {
+	// s가 비어있다면 그냥 0 return  
+	if (s.empty()) return 0;
+	// 존재하면 1 return 
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == x) {
+			return 1;
+		}
+	}
+	// 존재하지 않으면 0 return 
+	return 0;
+}
+
 void add(int x) {
-	// s 사이즈 0이면 그냥 추가 
-	if (s.size() == 0) {
+	// s에 아무것도 없다면 x 그냥 추가 
+	if (s.empty()) {
 		s.push_back(x);
 		return;
 	}
 	// x가 이미 존재하면 연산 무시
-	for (int i=0;i<s.size();i++) {
-		if (s[i] == x) return;
-	}
+	if (check(x)) return;
 	// x 추가 
 	s.push_back(x);
 }
 
-void remove() {
+void remove(int x) {
 	// s 사이즈 0이면 아무 작업 X
 	if (s.size() == 0) {
 		return;
 	}
 	// x 없애기 
-	for (int i=0;i<s.size();i++) {
-		if (s[i] == x) {
-			s.erase(s.begin() + i);
-		} 
+	if (check(x)) {
+		s.erase(std::remove(s.begin(), s.end(), x));
+		return;
 	}
-	
 	// x가 존재하지 않으면 연산 무시 
 	return;
 }
 
-bool check() {
-
-}
-
-void toggle() {
-	
-}
-
-void all() {
-	
+void toggle(int x) {
+	// x 존재하면 x 제거 
+	if (check(x)) remove(x);
+	// x 존재하지 않으면 x 추가 
+	else add(x);
 }
 
 void empty() {
-	
+	s.clear(); // 공집합으로 바꾸기 
+}
+
+void all() {
+	empty(); // s를 공집합으로 바꾼 후 
+	for (int i = 1; i <= 20; i++) { // 1부터 20까지 넣어주기 
+		s.push_back(i);
+	}
 }
 
 int main() {
 	int m;
 	cin >> m;
-	
+
 	string cmd;
-	int x; 
-	for (int i=0;i<m;i++) {
-		cin >> cmd >> x;
-		if (cmd == "add") add(x);
-		if (cmd == "remove") remove();
-		if (cmd == "check") check();
-		if (cmd == "toggle") toggle();
-		if (cmd == "all") all();
-		if (cmd == "empty") empty();
+	int x;
+	for (int i = 0; i < m; i++) {
+		cin >> cmd;
+		if (cmd == "add") {
+			cin >> x;
+			add(x);
+			continue;
+		}
+		if (cmd == "remove") {
+			cin >> x;
+			remove(x);
+			continue;
+		}
+		if (cmd == "check") {
+			cin >> x;
+			cout << check(x) << "\n";
+			continue;
+		}
+		if (cmd == "toggle") {
+			cin >> x;
+			toggle(x);
+			continue;
+		}
+		if (cmd == "all") {
+			all();
+			continue;
+		}
+		if (cmd == "empty") {
+			empty();
+			continue;
+		}
 	}
-	for (int i=0;i<s.size();i++) {
-			cout << s[i];
-	}
-	
 	return 0;
-} 
+}
