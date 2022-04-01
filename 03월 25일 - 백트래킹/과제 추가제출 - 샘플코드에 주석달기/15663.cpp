a01450d2a4b368ab111aa774c609c3c6a893b28e
CSE-pebble <CSE.pebble@gmail.com>
2022-04-01 22:05:33 +0900

    [백트래킹] 4월 1일
    15663.cpp

---
+++ 03월 25일 - 백트래킹/과제 추가제출 - 샘플코드에 주석달기/15663.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 8; //배열 크기
+
+int n, m;
+vector<int> num(SIZE);
+vector<int> ans(SIZE);
+vector<bool> check(SIZE); 
+
+void backtracking(int cnt) {
+    if (cnt == m) { //길이 m 이라면 (기저조건)
+        for (int i = 0; i < m; i++) {
+            cout << ans[i] << ' '; // m개의 수를 모두 고른 것이므로 출력 
+        }
+        cout << '\n';
+        return;
+    }
+    int prev = 0; //이전에 선택한 값
+    for (int i = 0; i < n; i++) {
+        if (!check[i] && prev != num[i]) { //수 위치 중복 체크 && 이전에 탐색한 값이 아니라면
+            prev = num[i]; // prev 값 업데이트 
+            ans[cnt] = num[i]; // 답에 저장 
+            check[i] = true; // check 배열 업데이트해서 중복 방지 
+            backtracking(cnt + 1); // 그 다음 수 고르기 
+            check[i] = false; // 원래대로 돌려주기 
+        }
+    }
+}
+
+/**
+ * 1. 우선 수열을 사전 순으로 증가하는 순서로 출력해야 하므로 주어진 수열을 오름차순 정렬
+ * 2. 이 상태에서 우선, 같은 위치의 수를 또 선택하지 않도록 중복제거 (check 배열 사용)를 해줌
+ * 3. 그 후, 중복되는 "수열"을 거르는 방법은 이전에 선택한 값을 변수에 저장해서, 수열을 만들다 다시 루트 노드로 돌아왔을 때
+ *    이전에 선택한 값과 같은 값이면 넘어가면 됨! -> 어차피 같은 수이므로 같은 과정 반복해서 똑같은 수열이 나올 것
+ */
+
+int main() {
+    //입력
+    cin >> n >> m;  
+    for (int i = 0; i < n; i++) {
+        cin >> num[i]; 
+    }
+
+    //연산
+    sort(num.begin(), num.begin() + n); // 사전순 출력을 위해 오름차순 정렬 
+
+    //백트래킹 & 출력
+    backtracking(0);
+    return 0;
+}
