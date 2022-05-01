56889d022356ad0c6e5ee78024402fe29c252109
CSE-pebble <CSE.pebble@gmail.com>
2022-05-01 23:31:37 +0900

    [DFS & BFS] 5월 1일
    11725.cpp

---
+++ 04월 15일 - DFS & BFS/과제 추가제출 - 샘플코드에 주석달기/11725.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> adj_list;
+vector<int> parent;
+
+void dfs_recursion(int prev, int curr) {
+    if (parent[curr]) { // 현재 노드의 부모가 존재하면  
+        return; // return
+    }
+    parent[curr] = prev; // 현재 노드의 부모는 이전 노드 
+    for (int i = 0; i < adj_list[curr].size(); i++) {
+        dfs_recursion(curr, adj_list[curr][i]); // 현재 노드의 부모 노드 또 찾기 재귀함수  
+    }
+}
+
+/**
+ * [트리의 부모 찾기]
+ *
+ * 1번 노드에서 출발해서 탐색
+ * 루트 노드에서 출발 했기 때문에, 현재 노드의 부모는 이전 노드가 된다.
+ */
+
+int main() {
+    int n, a, b;
+
+    //입력
+    cin >> n; // 노드의 개수  
+    adj_list.assign(n + 1, vector<int>(0));
+    parent.assign(n + 1, 0);
+    for (int i = 1; i < n; i++) {
+        cin >> a >> b; // 연결된 두 정점 
+        adj_list[a].push_back(b); // 연결 노드에 모두 연결시키기   
+        adj_list[b].push_back(a); // 연결 노드에 모두 연결시키기 
+    }
+
+    //연산
+    dfs_recursion(1, 1); //1번 노드는 루트노드이므로, 부모를 자기 자신으로 설정
+
+    //출력
+    for (int i = 2; i <= n; i++) {
+        cout << parent[i] << '\n';  
+    }
+    return 0;
+}
---
+++ 04월 15일 - DFS & BFS/과제 추가제출 - 샘플코드에 주석달기/1697.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+const int SIZE = 1e5;
+
+int bfs(int n, int k) {
+    vector<int> check(SIZE + 1, 0);
+    queue<int> q; //큐에 좌표값 저장
+    int ans = 0;
+
+    check[n] = 1; //시작 노드 방문체크 + 시간 초기화
+    q.push(n); // 수빈이 위치 큐에 넣기 
+    while (!q.empty()) {
+        int x = q.front(); // 해당 좌표를 x값으로 설정 
+        q.pop(); // x값에 넣었으니 pop 
+
+        if (x == k) { // 동생 찾으면  
+            ans = check[x] - 1; // ans 업데이트 
+            break; // 반복문 빠져나가기 
+        }
+
+        vector<int> child = {x - 1, x + 1, x * 2}; //자식 노드
+        for (int i = 0; i < 3; i++) { // 움직일 수 있는 곳 3곳 
+            if (child[i] >= 0 && child[i] <= SIZE && !check[child[i]]) { // 좌표 이동하며 동생 위치 찾기 
+                check[child[i]] = check[x] + 1; // 시간 업데이트 
+                q.push(child[i]); // q 업데이트 
+            }
+        }
+    }
+    return ans;
+}
+
+/**
+ * [숨바꼭질]
+ *
+ * x좌표 위에서 x-1, x+1, 2*x의 위치로 계속 이동하며 탐색
+ * 가장 빠른 시간을 찾아야 하므로 주변 노드부터 탐색하는 풀이로 풀어서 k에 도달하면 바로 탐색 종료 (bfs)
+ */
+
+int main() {
+    int n, k; 
+
+    //입력
+    cin >> n >> k; // 수빈이 위치, 동생 위치 
+
+    //연산 & 출력
+    cout << bfs(n, k);
+    return 0;
+}
